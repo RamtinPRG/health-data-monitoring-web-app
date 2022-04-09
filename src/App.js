eb9d4327adff5bcfe13547e7f476d91886aee3b0
RamtinPRG <ramtin.barzkar.3@gmail.com>
2022-04-09 23:41:26 +0430

    Implement authentication context and add basic app pages and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/AuthContext";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Page404 from "./pages/Page404";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <AuthContextProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="*" element={<Page404 />} />
+          </Routes>
+        </AuthContextProvider>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,104 @@
+import { useState, useEffect, createContext } from "react";
+import { useNavigate } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+
+const AuthContext = createContext();
+
+export default AuthContext;
+
+export function AuthContextProvider({ children }) {
+  const [authTokens, setAuthTokens] = useState(() =>
+    localStorage.getItem("authTokens")
+      ? JSON.parse(localStorage.getItem("authTokens"))
+      : null
+  );
+  const [user, setUser] = useState(() =>
+    localStorage.getItem("authTokens") ? jwtDecode(authTokens.access) : null
+  );
+  const [loading, setLoading] = useState(true);
+
+  const navigate = useNavigate();
+
+  const loginUser = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8000/api/token/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: e.target.username.value,
+        password: e.target.password.value,
+      }),
+    });
+    const data = await response.json();
+    if (response.status === 200) {
+      setAuthTokens(data);
+      setUser(jwtDecode(data.access));
+      localStorage.setItem("authTokens", JSON.stringify(data));
+      navigate("/");
+    } else {
+      alert("Something went wrong!");
+    }
+  };
+
+  const logoutUser = () => {
+    setAuthTokens(null);
+    setUser(null);
+    localStorage.removeItem("authTokens");
+    navigate("/login");
+  };
+
+  const updateToken = async () => {
+    console.log("token");
+    if (authTokens) {
+      const response = await fetch("http://localhost:8000/api/token/refresh/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          refresh: authTokens?.refresh,
+        }),
+      });
+      const data = await response.json();
+      if (response.status === 200) {
+        setAuthTokens(data);
+        setUser(jwtDecode(data.access));
+        localStorage.setItem("authTokens", JSON.stringify(data));
+      } else {
+        logoutUser();
+      }
+    }
+    if (loading) {
+      setLoading(false);
+    }
+  };
+
+  const contextData = {
+    user,
+    authTokens,
+    loginUser,
+    logoutUser,
+  };
+
+  useEffect(() => {
+    if (loading) {
+      updateToken();
+    }
+    const interval = setInterval(() => {
+      if (authTokens) {
+        updateToken();
+      }
+    }, 1000 * 60 * 4);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [authTokens, loading]);
+
+  return (
+    <AuthContext.Provider value={contextData}>
+      {loading ? null : children}
+    </AuthContext.Provider>
+  );
+}
